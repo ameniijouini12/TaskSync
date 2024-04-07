ea352373e2b4bb9fe4ba1454d3eb8e875e713f6e
hhannachi <hazem.hannachi@sofrecom.com>
2024-04-07 23:37:07 +0100

    first commit

---
+++ MS_Microservice_Gestion_Comment/src/main/java/com/example/MS_Microservice_Gestion_Comment/MsMicroserviceGestionCommentApplication.java
@@ -0,0 +1,13 @@
+package com.example.MS_Microservice_Gestion_Comment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMicroserviceGestionCommentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMicroserviceGestionCommentApplication.class, args);
+	}
+
+}
